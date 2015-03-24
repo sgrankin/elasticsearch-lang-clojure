49f8a590c395f0b4a6016168d7d3398f7674cd6c
Sergey Grankin <sgrankin@gmail.com>
2015-03-24 00:51:42 -0400

    update to elasticsearch 1.5.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]]
-  :profiles {:provided {:dependencies [[org.elasticsearch/elasticsearch "1.4.4"]]}
+  :profiles {:provided {:dependencies [[org.elasticsearch/elasticsearch "1.5.0"]]}
              :uberjar {:aot :all}
              :dev {:dependencies [[log4j/log4j "1.2.17"]]}}
   :aot :all)
