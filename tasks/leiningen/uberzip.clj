344bb6d57ca6e5f5add4df9ab939b4c16a494088
Sergey Grankin <sgrankin@gmail.com>
2015-06-10 23:46:27 -0400

    fix the uberzip task

--- tasks/leiningen/uberzip.clj
+++ tasks/leiningen/uberzip.clj
@@ -1,21 +1,25 @@
 (ns leiningen.uberzip
-  (:require [leiningen.jar :refer [get-jar-filename]]
-            [leiningen.core.main :refer [info]]
-            [clojure.java.io :refer [input-stream copy file]]
-            )
-  (:import [java.io FileOutputStream]
-           [java.util.zip ZipOutputStream ZipEntry]))
+  (:require
+    [clojure.java.io :as io]
+    [leiningen.core.main :refer [apply-task info]]
+    [leiningen.jar :refer [get-jar-filename]]
+    )
+  (:import
+    [java.io FileOutputStream]
+    [java.util.zip ZipOutputStream ZipEntry]
+    ))
 
 (defn uberzip [project & args]
   "Zip the uberjar"
-  (let [f (get-jar-filename project :standalone)
-        {v :version n :name } project]
-    (with-open [out (-> (str "target/" n "-" v ".zip")
-                        (FileOutputStream.)
-                        (ZipOutputStream.))]
-      (with-open [in (input-stream f)]
-        (.putNextEntry out (ZipEntry. (-> f file .getName)))
-        (copy in out)
-        (.closeEntry out)
-        (info "zipped file" f)))
+  (apply-task "uberjar" project args)
+  (let [jar (io/file (get-jar-filename project :standalone))
+        {v :version n :name} project
+        zip (.getPath (io/file (.getParent jar) (str n "-" v ".zip")))
+        ]
+    (with-open [out (-> zip io/output-stream ZipOutputStream.)
+                in (io/input-stream jar)]
+      (.putNextEntry out (-> jar .getName ZipEntry.))
+      (io/copy in out)
+      (.closeEntry out)
+      (info "Created " zip))
     ))
